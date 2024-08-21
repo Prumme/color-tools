587cf31dc3eb2f0ccf0b808887a6e904133904dc
Prumme <aurelien23.p@gmail.com>
2024-08-21 09:57:01 +0200

    update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,12 +5,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/color-tools/",
   optimizeDeps: {
-    exclude: ['js-big-decimal']
+    exclude: ["js-big-decimal"],
   },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});
